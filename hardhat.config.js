d56439e236813e94ca864811ef7921386aa33097
Pranav2612000 <pranav2000joglekar@gmail.com>
2021-12-20 21:51:56 +0530

    feat: use env variables and publish contract code on etherscan

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,6 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -20,8 +22,11 @@ module.exports = {
   solidity: "0.8.0",
   networks: {
     rinkeby: {
-      url: '<secret-url-here>',
-      accounts: ['<secret-private-key-here>'],
+      url: process.env.STAGING_ALCHEMY_KEY,
+      accounts: [process.env.PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  }
 };
